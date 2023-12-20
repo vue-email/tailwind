966f5ce6b363d0b102ca45e15c31a1ed697d9894
Younes Barrad <younesbarraddev@gmail.com>
2023-12-20 18:03:45 +0100

    feat: added load config

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,6 @@
 import type { Config } from 'tailwindcss'
 
-export interface TailwindConfig {
-  important?: Config['important']
-  prefix?: Config['prefix']
-  separator?: Config['separator']
-  safelist?: Config['safelist']
-  presets?: Config['presets']
-  future?: Config['future']
-  experimental?: Config['experimental']
-  darkMode?: Config['darkMode']
-  theme?: Config['theme']
-  corePlugins?: Config['corePlugins']
-  plugins?: Config['plugins']
-}
+export type TailwindConfig = Partial<Config>
 
 export function cssToJsxStyle(cssText: string): Record<string, string>
 export function escapeClassName(className: string): string
