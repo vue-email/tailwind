51a70e7144ac315d68baccbfd4b7b50e6b450f76
Younes Barrad <younesbarraddev@gmail.com>
2023-12-20 18:08:46 +0100

    chore: updated types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,20 @@
 import type { Config } from 'tailwindcss'
 
-export type TailwindConfig = Partial<Config>
-
+export interface TailwindConfig {
+  content?: Config['content']
+  purge?: Config['purge']
+  important?: Config['important']
+  prefix?: Config['prefix']
+  separator?: Config['separator']
+  safelist?: Config['safelist']
+  presets?: Config['presets']
+  future?: Config['future']
+  experimental?: Config['experimental']
+  darkMode?: Config['darkMode']
+  theme?: Config['theme']
+  corePlugins?: Config['corePlugins']
+  plugins?: Config['plugins']
+}
 export function cssToJsxStyle(cssText: string): Record<string, string>
 export function escapeClassName(className: string): string
 export function getCssForMarkup(markup: string, config: TailwindConfig | undefined): Promise<string>
